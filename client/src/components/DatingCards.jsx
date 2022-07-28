7255d2e60d5e93f2ed78092427177d6594a6805c
José Mota <jose.a.mota11@gmail.com>
2022-07-28 13:36:48 -0400

    Backend completed

--- client/src/components/DatingCards.jsx
+++ client/src/components/DatingCards.jsx
@@ -1,14 +1,19 @@
 import "./DatingCards.css"
-import { useState } from "react"
+import { useState, useEffect } from "react"
 import DatingCard from "react-tinder-card"
+import axios from "axios"
 
 function DatingCards() {
-  const [people, setPeople] = useState([
-    { name: "Random Guy", imgUrl: "https://images.unsplash.com/photo-1508341591423-4347099e1f19?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=687&q=80" },
-    { name: "Another Guy", imgUrl: "https://images.unsplash.com/photo-1492447273231-0f8fecec1e3a?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=687&q=80" },
-    { name: "Random Girl", imgUrl: "https://images.unsplash.com/photo-1524250502761-1ac6f2e30d43?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=688&q=80" },
-    { name: "Another Girl", imgUrl: "https://images.unsplash.com/photo-1464863979621-258859e62245?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=686&q=80" }
-  ])
+
+  const [people, setPeople] = useState([])
+
+  useEffect(() => {
+    async function fetchData() {
+      const req = await axios.get("http://127.0.0.1:8001/dating/cards")
+      setPeople(req.data)
+    }
+    fetchData()
+  }, [])
 
   const swiped = (direction, nameToDelete) => {
     console.log("receiving " + nameToDelete)
@@ -40,4 +45,3 @@ function DatingCards() {
 }
 
 export default DatingCards
-// Page 43
